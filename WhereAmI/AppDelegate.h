58bba2ab07d00a383b4430fa5e4e365b57015545
tetujin <tetujin@ht.sfc.keio.ac.jp>
2015-11-09 19:13:58 -0500

    Initial Commit

---
+++ WhereAmI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhereAmI
+//
+//  Created by Yuuki Nishiyama on 11/9/15.
+//  Copyright © 2015 Yuuki NISHIYAMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhereAmI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhereAmI
+//
+//  Created by Yuuki Nishiyama on 11/9/15.
+//  Copyright © 2015 Yuuki NISHIYAMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
